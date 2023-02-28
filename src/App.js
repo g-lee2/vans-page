afae2163475b8652cce339f99bb90ac95ac21f52
Grace Lee <gracelee9901@gmail.com>
2023-02-27 20:16:28 -0800

    Create Vans page and route with api call to get van info

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
+import Vans from './components/Vans';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <header>
+        <Link className="site-logo" to="/">#VANLIFE</Link>
         <nav>
-          <Link className="site-logo" to="/">#VANLIFE</Link>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
       </nav>
       </header>
-      
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
+          <Route path="/vans" element={<Vans />}/>
         </Routes>
       </BrowserRouter>
   );
