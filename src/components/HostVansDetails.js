2f3e2a5c9d1d3b6179005a137c345ac9ae094e9b
Grace Lee <gracelee9901@gmail.com>
2023-03-02 23:55:27 -0800

    Build out Host Vans Details page

--- src/components/HostVansDetails.js
+++ src/components/HostVansDetails.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function HostVansDetails() {
-    return (
-        <h1>HOST VANS DETAILS PAGE</h1>
-    );
+  const params = useParams();
+  const [hostVanDetail, setHostVanDetail] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${params.id}`)
+      .then(res => res.json())
+      .then(data => setHostVanDetail(data.vans))
+  }, []);
+
+  if (!hostVanDetail) {
+    return <h1>Loading...</h1>
+  } 
+  
+  return ( 
+    <div>
+      <img src={hostVanDetail.imageUrl} width={150} />
+      <h2>{hostVanDetail.name}</h2>
+      <p>{hostVanDetail.price}</p>
+      <p>{hostVanDetail.type}</p>
+    </div>
+  );
 }
 
 export default HostVansDetails;
