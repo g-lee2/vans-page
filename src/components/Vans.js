f73e0caad2aedf3e2a383c9bc60e65dd1124b940
Grace Lee <gracelee9901@gmail.com>
2023-03-08 19:10:39 -0800

    Add search params

--- src/components/Vans.js
+++ src/components/Vans.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 function Vans() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = useState([]);
+
+  const typeFilter = searchParams.get("type");
 
   useEffect(() => {
     fetch("/api/vans")
@@ -10,7 +13,9 @@ function Vans() {
       .then(data => setVans(data.vans))
   },[]);
 
-  const vanElements = vans.map(van => (
+  const filteredVans = typeFilter ? vans.filter(van => van.type.toLowerCase() === typeFilter) : vans;
+
+  const vanElements = filteredVans.map(van => (
     <div key={van.id} className="van-tile">
       <Link to={`/vans/${van.id}`}>
         <img src={van.imageUrl} />
