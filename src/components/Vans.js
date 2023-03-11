f13c1d692fac6547edc0b8c8c6b0cd6ba4b8dbcd
Grace Lee <gracelee9901@gmail.com>
2023-03-11 14:43:30 -0800

    Update to handle errors and fetch van data outside of useeffect hook

--- src/components/Vans.js
+++ src/components/Vans.js
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
+import { getVans } from '../api';
 
 function Vans() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const typeFilter = searchParams.get("type");
 
   useEffect(() => {
-    fetch("/api/vans")
-      .then(res => res.json())
-      .then(data => setVans(data.vans))
-  },[]);
+    async function loadVans() {
+      setLoading(true);
+      try {
+        const data = await getVans();
+        setVans(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadVans();
+  }, []);
 
   const filteredVans = typeFilter ? vans.filter(van => van.type.toLowerCase() === typeFilter) : vans;
 
@@ -27,6 +39,14 @@ function Vans() {
       </Link>
     </div>
   )); 
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  if (error) {
+    return <h1>There was an error: {error.message}</h1>
+  }
 
   return (
     <div className="van-list-container">
