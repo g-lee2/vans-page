14f0f6cf24a728efdf88d754a860191a1eeeab60
Grace Lee <gracelee9901@gmail.com>
2023-03-12 15:29:58 -0700

    Use userouteerror hook to display error message and status

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 function Error() {
-  return <h1>An error occured!</h1>
+  const error = useRouteError();
+  return (
+    <div>
+      <h1>Error: {error.message}</h1>
+      <pre>{error.status} - {error.statusText}</pre>
+    </div>
+  )
 }
 
 export default Error;
