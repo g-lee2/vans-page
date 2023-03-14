045b3c78a2087ced606a68d35c75e5ea3457e744
Grace Lee <gracelee9901@gmail.com>
2023-03-13 23:08:27 -0700

    Add login page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import HostVansPricing from './components/HostVansPricing';
 import HostVansInfo from './components/HostVansInfo';
 import NotFound from './components/NotFound';
 import Error from './components/Error';
+import Login from './components/Login';
 
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
@@ -22,6 +23,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />}/>
     <Route path="about" element={<About />}/>
+    <Route path="login" element={<Login />} />
     <Route path="vans" element={<Vans />} loader={vansLoader} errorElement={<Error />}/>
     <Route path="vans/:id" element={<VanDetail />}/>
     <Route path="host" element={<HostLayout />}>
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import { createServer, Model } from "miragejs";
+import { createServer, Model, Response } from "miragejs";
 
 
 createServer({
@@ -18,25 +18,27 @@ createServer({
     routes() {
         this.namespace = "api";
         this.logging = false;
+        // this.timing = 2000
 
         this.get("/vans", (schema, request) => {
-            return schema.vans.all()
-        });
+            // return new Response(400, {}, {error: "Error fetching data"})
+            return schema.vans.all();
+        })
 
         this.get("/vans/:id", (schema, request) => {
-            const id = request.params.id
-            return schema.vans.find(id)
-        });
+            const id = request.params.id;
+            return schema.vans.find(id);
+        })
 
         this.get("/host/vans", (schema, request) => {
             // Hard-code the hostId for now
-            return schema.vans.where({ hostId: "123" })
-        });
+            return schema.vans.where({ hostId: "123" });
+        })
 
         this.get("/host/vans/:id", (schema, request) => {
             // Hard-code the hostId for now
-            const id = request.params.id
-            return schema.vans.findBy({ id, hostId: "123" })
+            const id = request.params.id;
+            return schema.vans.findBy({ id, hostId: "123" });
         });
     }
 });
