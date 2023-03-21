1b66127d5f533391bef7460cd9a0e40f3c5b915f
Grace Lee <gracelee9901@gmail.com>
2023-03-20 23:36:10 -0700

    Use data in action to log in and add error handling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation, useNavigate, Form } from "react-router-dom";
+import { useLocation, useNavigate, Form, useActionData } from "react-router-dom";
 import { loginUser } from "../api";
 
 
@@ -8,63 +8,43 @@ export async function action({ request }) {
   const email = formData.get("email");
   const password = formData.get("password");
   console.log(email, password);
+  try {
+    const data = await loginUser({email, password});
+    localStorage.setItem("loggedin", true);
+    return data;
+  } catch(err) {
+    return {
+      error: err.message
+    }
+  }
 }
 
 export default function Login() {
-  const [loginFormData, setLoginFormData] = useState({ email: "", password: "" });
-  
   const [status, setStatus] = useState("idle");
-  const [error, setError] = useState(null);
-
+  const data = useActionData();
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from || "/host";
   
-  function handleSubmit(e) {
-    e.preventDefault();
-    setStatus("submitting");
-    setError(null);
-    loginUser(loginFormData)
-      .then(data => {
-        console.log(data);
-        localStorage.setItem("loggedin", true);
-        navigate(from, { replace: true });
-      })
-      .catch(err => {
-        setError(err);
-      })
-      .finally(() => {
-        setStatus("idle");
-      })
-  }
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setLoginFormData(prev => ({
-      ...prev,
-      [name]: value
-    }));
+  if (data?.token) {
+    navigate(from, { replace: true });
   }
 
   return (
     <div className="login-container">
       {location.state?.message && <h3>{location.state.message}</h3>}
       <h1>Sign in to your account</h1>
-      {error && <h3>{error.message}</h3>}
+      {data?.error && <h3>{data.error}</h3>}
       <Form action="/login" method="post" className="login-form">
         <input
           name="email"
-          onChange={handleChange}
           type="email"
           placeholder="Email address"
-          value={loginFormData.email}
         />
         <input
           name="password"
-          onChange={handleChange}
           type="password"
           placeholder="Password"
-          value={loginFormData.password}
         />
         <button disabled={status === "submitting"}>{status === "submitting" ? "Logging in..." : "Log in"}</button>
       </Form>
